b82d10a3310a6aa6dc141c1b066d9cc2292c681c
cold271 <4i6oon18@gmail.com>
2025-02-21 19:22:08 +0200

    Update call.php

--- call.php
+++ call.php
@@ -78,8 +78,8 @@ class prankCall{
 	private function randStr($l){
 		$data = "abcdefghijklmnopqrstuvwxyz1234567890";
 		$word = "";
-		for($a=0;$a<$l;$a++){
-			$word .= $data{rand(0,strlen($data)-1)};
+		for($a=0;$a<$l;$a++)[
+			$word .= $data{rand(0,strlen($data)-1)];
 		}
 		return $word;
 	}
